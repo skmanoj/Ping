483f636e32fa6d873488cf7f96c8b3aa7577993e
manoj <skmanoj1988@gmail.com>
2015-04-25 23:53:04 +0530

    Taking advantage of multi core systems, launching cluster of node processes to handle load

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-var express = require('express'),
-	app     = express(),
-	route	= require('./routes'),
-	config	= require('./config');
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length;
 
-var io = require('socket.io').listen(app.listen(config.port, function() {
+if (cluster.isMaster) {
+  	
+  	// Fork workers.
+  	for (var i = 0; i < numCPUs; i++) {
+    	cluster.fork();
+  	}
+
+  	cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  	});
+} else {
+	var express = require('express'),
+	    app     = express(),
+	    route	= require('./routes'),
+	    config	= require('./config');
+
+	var io = require('socket.io').listen(app.listen(config.port, function() {
 	console.log('connected to Ping chat server at port: ', config.port);
-}));
+	}));
 
-config.init(app);
-route(app, io);
+	config.init(app);
+	route(app, io);
+}
