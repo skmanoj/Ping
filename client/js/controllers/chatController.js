13a0e4e85b72e5e41591c4b390c977827a424241
manoj <skmanoj1988@gmail.com>
2015-05-03 00:02:47 +0530

    bootstrapping chat page

--- client/js/controllers/chatController.js
+++ client/js/controllers/chatController.js
@@ -166,7 +166,6 @@ chatApp.controller('BasicChatController', ['$scope', 'socketio', '$location', '$
 }]);
 
 function isValid(thatemail) {
-
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(thatemail);
 }
