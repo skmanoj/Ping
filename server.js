0ccb7c313c7e3cc8259c0542fbc50bfcaff4cbe4
manoj <skmanoj1988@gmail.com>
2015-05-02 10:58:22 +0530

    fixing loading issue in chat

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ if (cluster.isMaster) {
     	cluster.fork();
   	}
 
+  	cluster.on('online', function(worker) {
+  		console.log("The worker "+worker.process.pid+ "responded after it was forked");
+	});
+
   	cluster.on('exit', function(worker, code, signal) {
     	console.log('worker ' + worker.process.pid + ' died');
   	});
 } else {
+
+	//this is run by worker's which are forked
 	var express = require('express'),
 	    app     = express(),
 	    route	= require('./routes'),
 	    config	= require('./config');
+
+	//console.log('I am worker #' + cluster.worker.id);
 
 	var io = require('socket.io').listen(app.listen(config.port, function() {
 		console.log('connected to Ping chat server at port: ', config.port);
