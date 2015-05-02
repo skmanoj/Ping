27cdd763e9f47ef7af7680ce22fe292a25ff5912
manoj <skmanoj1988@gmail.com>
2015-05-02 11:15:07 +0530

    socket.io not supporting multiple node processes, fixing issue

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
-var cluster = require('cluster'),
-    numCPUs = require('os').cpus().length;
+var express = require('express'),
+    app     = express(),
+    route	= require('./routes'),
+    config	= require('./config');
 
-if (cluster.isMaster) {
-  	
-  	// Fork workers.
-  	for (var i = 0; i < numCPUs; i++) {
-    	cluster.fork();
-  	}
 
-  	cluster.on('online', function(worker) {
-  		console.log("The worker "+worker.process.pid+ "responded after it was forked");
-	});
+var io = require('socket.io').listen(app.listen(config.port, function() {
+	console.log('connected to Ping chat server at port: ', config.port);
+}));
 
-  	cluster.on('exit', function(worker, code, signal) {
-    	console.log('worker ' + worker.process.pid + ' died');
-  	});
-} else {
-
-	//this is run by worker's which are forked
-	var express = require('express'),
-	    app     = express(),
-	    route	= require('./routes'),
-	    config	= require('./config');
-
-	//console.log('I am worker #' + cluster.worker.id);
-
-	var io = require('socket.io').listen(app.listen(config.port, function() {
-		console.log('connected to Ping chat server at port: ', config.port);
-	}));
-
-	config.init(app);
-	route(app, io);
-}
+config.init(app);
+route(app, io);
